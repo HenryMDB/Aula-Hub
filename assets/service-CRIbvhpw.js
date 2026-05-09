e426061197ce83f00a595d4272dd7740c94c2b88
Henry <ngq.datt@#gmail.com>
2026-05-09 22:27:23 +0700

    Update V3

--- assets/service-CRIbvhpw.js
+++ assets/service-CRIbvhpw.js
@@ -1,9 +1,13 @@
 import { a as o } from "./index-C0Zqfgkc.js";
-const a = "https://hubapi.aulacn.com/",
+
+// ĐỔI DÒNG NÀY: Thay bằng link Cloudflare Worker của ông
+// Ví dụ: const a = "https://cors-proxy.ten-cua-ong.workers.dev/",
+const a = "https://cors-proxy-api.henrymdb.workers.dev/",
   t = o.create({ baseURL: a, timeout: 2e4 });
+
 t.interceptors.request.use(
   (e) => e,
-  (e) => (console.error(e), Promise.reject(e))
+  (e) => (console.error(e), Promise.reject(e)),
 );
 t.interceptors.response.use(
   (e) => {
@@ -12,6 +16,6 @@ t.interceptors.response.use(
       ? Promise.resolve(r || e.data)
       : Promise.reject(r);
   },
-  (e) => (console.error("err" + e), Promise.reject(e))
+  (e) => (console.error("err" + e), Promise.reject(e)),
 );
 export { t as s };
